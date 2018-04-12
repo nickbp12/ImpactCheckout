7f5891f4b9255c0477f4a0f05d45a802e86afc47
VID\mart390g <martinokuhnell@gmail.com>
2018-04-12 09:38:45 +0200

    radio check js++

---
+++ WebApp/Scripts/main.js
@@ -0,0 +1,5 @@
+﻿$(".checkout_box_radio-button").click(function () {
+    $(this).find("input[type='radio']").prop("checked", true);
+    $(this).addClass("checkout_box_radio-button--checked");
+    $(this).siblings().removeClass("checkout_box_radio-button--checked");
+});
